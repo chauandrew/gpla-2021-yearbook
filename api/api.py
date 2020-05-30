68d3216ef7b5b2057eec4c52b90c76b669e5689d
Ubuntu <ubuntu@ip-172-31-16-19.us-west-1.compute.internal>
2020-05-30 09:18:15 +0000

    Initial port to flask, gave up on apache / nginx lol

--- api/api.py
+++ api/api.py
@@ -1,12 +1,31 @@
-import flask
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+import pymongo
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = Flask(__name__)
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = client["FlaskApp"]
+posts = mydb["Posts"]
 
+@app.route("/insert", methods=['POST'])
+def insert():
+    # TODO: type checking, turn single length arrays into standalone objects, etc.
+    args = request.args.to_dict(flat=False)
+    print(args)
+    match = {'id': args['id']}
+    x = posts.update(match, args, upsert=True)
+    return request.args
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+@app.route("/remove", methods=['POST'])
+def remove():
+    args = request.args.to_dict(flat=False)
+    match = {}
+    for key, value in args.items():
+        match[key] = value
+    ok = posts.delete_one(match)
+    print(match, ok)
+    return request.args
+            
 
-app.run()
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
