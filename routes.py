70f77de09179139c8d80008b8fcb30632640e3f7
Andrew Chau <andrewchau333@gmail.com>
2020-06-01 20:19:31 -0700

    Fixed path expansion

--- routes.py
+++ routes.py
@@ -15,7 +15,7 @@ from bson import ObjectId
 # client = pymongo.MongoClient("127.0.0.1:27017") # Local
 
 # Production Settings
-UPLOAD_FOLDER = f"{os.path.expanduser()}/image-data"
+UPLOAD_FOLDER = f"{os.path.expanduser('~')}/image-data"
 client = pymongo.MongoClient("mongodb+srv://holynugget:KdYcnS5LWJgAchDI@cluster0-fuwyc.mongodb.net/test?retryWrites=true&w=majority")
 
 PAGE_SIZE=3
