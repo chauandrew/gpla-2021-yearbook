0ca39016162bfce01d0af7ae5a8ba63839d52d17
Jiaping Zeng <jzeng@outlook.com>
2020-06-04 03:10:27 -0700

    added server album feed; use env var in config

--- config.py
+++ config.py
@@ -4,17 +4,19 @@ import os
 # global UPLOAD_FOLDER
 # global DBCLIENT
 
-# Local Settings
-# UPLOAD_FOLDER = f"{os.path.expanduser('~')}/testing" # Local
-# client = pymongo.MongoClient("127.0.0.1:27017") # Local
-# file = f"{os.path.expanduser('~')}/.tinypng_key.txt"
-# COMPRESSION_KEY = ""
-# with open(file, 'r') as file:
-#     COMPRESSION_KEY = file.read().replace('\n', '')
-
-# Production Settings
-UPLOAD_FOLDER = f"{os.path.expanduser('~')}/image-data"
-client = pymongo.MongoClient("mongodb+srv://holynugget:KdYcnS5LWJgAchDI@cluster0-fuwyc.mongodb.net/test?retryWrites=true&w=majority")
-COMPRESSION_KEY = os.environ['TINYPNG_KEY']
+if 'PROD_ENV' in os.environ and os.environ['PROD_ENV']:
+	# Production Settings
+	UPLOAD_FOLDER = f"{os.path.expanduser('~')}/image-data"
+	client = pymongo.MongoClient("mongodb+srv://holynugget:KdYcnS5LWJgAchDI@cluster0-fuwyc.mongodb.net/test?retryWrites=true&w=majority")
+	COMPRESSION_KEY = os.environ['TINYPNG_KEY']
+else:
+	# Local Settings
+	UPLOAD_FOLDER = f"{os.path.expanduser('~')}/testing" # Local
+	client = pymongo.MongoClient("localhost:27017") # Local
+	#file = f"{os.path.expanduser('~')}/.tinypng_key.txt"
+	COMPRESSION_KEY = ""
+	#with open(file, 'r') as file:
+	#	COMPRESSION_KEY = file.read().replace('\n', '')
+	
 
 DBCLIENT = client["FlaskApp"]
--- www.py
+++ www.py
@@ -1,4 +1,6 @@
 from flask import render_template, make_response, Blueprint
+from api import find_by_quarter, findall
+import json
 
 www = Blueprint('www', __name__)
 
@@ -14,7 +16,17 @@ def index():
 def feed(quarter):
     if quarter not in ['fall', 'winter', 'spring']:
         return make_response("Quarter must be 'fall', 'winter' or 'spring'", 404)
-    return render_template(f"feed/feed.html", title=quarter)
+    
+    #posts = json.loads(find_by_quarter(quarter)) # this does not work atm, uncomment when find_by_quarter is fixed
+
+    posts = json.loads(findall())
+    for post in posts:
+        # TODO: add logic to append corresponding post comments
+        post['comments'] = ['test comment 1', 'another test comment that is also longer and will test how multiline looks']
+        # TODO: below adds images to test image/gallery posts, remove before publish
+        post['files'] = ['/static/postimages/testimage3.jpg', '/static/postimages/testimage2.jpg', '/static/postimages/testimage1.jpg']
+
+    return render_template(f"feed/feed.html", title=quarter, posts=posts)
 
 @www.route("/juniors")
 def juniors():
