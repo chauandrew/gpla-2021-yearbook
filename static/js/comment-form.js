b192df94218d952f33712469d7c66b67691bd94e
Jiaping Zeng <jzeng@outlook.com>
2020-06-04 18:07:26 -0700

    added check for empty comment

--- static/js/comment-form.js
+++ static/js/comment-form.js
@@ -4,27 +4,30 @@ $('.comment-form').on('submit', function (e) {
 	postid = $(this).attr('id').replace('comment-form-', '')
 	body = $(`#comment-input-${postid}`).val()
 
-	// add to comments, then ajax to server
-	$(`#comments-${postid}`).append(`<li class="list-group-item post-comment">${body}</li>`)
+	// only add if comment is not empty
+	if (body) {
+		// add to comments, then ajax to server
+		$(`#comments-${postid}`).append(`<li class="list-group-item post-comment">${body}</li>`)
 
-	$.ajax({
-		type: 'POST',
-		url: '/comment',
-		data: {
-			postid: postid,
-			body: body
-		},
-		success: function (message) {
-			toastr.success(message)
-		},
-		error: function (message) {
-			if (typeof message === 'string' || message instanceof String)
-				toastr.warning(message)
-			else
-				toastr.warning("An unknown error has occured")
-		}
-	})
+		$.ajax({
+			type: 'POST',
+			url: '/comment',
+			data: {
+				postid: postid,
+				body: body
+			},
+			success: function (message) {
+				toastr.success(message)
+			},
+			error: function (message) {
+				if (typeof message === 'string' || message instanceof String)
+					toastr.warning(message)
+				else
+					toastr.warning("An unknown error has occured")
+			}
+		})
 
-	// commented successfully, remove text from input
-	$(`#comment-input-${postid}`).val('')
+		// commented successfully, remove text from input
+		$(`#comment-input-${postid}`).val('')
+	}
 })
