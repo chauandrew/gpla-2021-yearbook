e62948bcc0f4f072148cce7308c9d6334abd31b9
Andrew Chau <andrewchau333@gmail.com>
2020-06-08 00:05:57 -0700

    Added prefetching on url hover

---
+++ static/js/prefetch.js
@@ -0,0 +1,15 @@
+prefetched = []
+
+// Prefetch urls the user hovers over
+$('a').on('mouseover', function() {
+    var href = $(this).attr("href")
+    if (prefetched.includes(href) || href.includes("/static/image")) {
+        return;
+    } else {
+        prefetched.push(href)
+    }
+    let link = document.createElement("link");
+    link.setAttribute("rel", "prefetch");
+    link.setAttribute("href", href);
+    document.head.appendChild(link);
+});
